cd670218d6e1e6c7fd11f2bb66c90a94e67bd24a
tiru.chilakamarri <tiru.chilakamarri@gmail.com>
2023-07-07 16:16:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const query = `
+{
+  blogPostCollection {
+    items {
+      title
+      image {
+        url
+      }
+    }
+  }
+}
+`;
 
 function App() {
+  const [page, setPage] = useState(null);
+  
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/kzg3llu8wx9t/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 3GEqLiW5Gotm6wn7zTGJ_SXIj5Q7p0smU7DKytSeWvk",
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+       setPage(data.blogPostCollection.items[0]);
+       console.log("Data from the object"+data.blogPostCollection);
+        console.log("Data::"+data.blogPostCollection.items[0].title);
+      });
+  }, []);
+
+  if (!page) {
+    return "Loading...";
+  }
+
+  // render the fetched Contentful data
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={page.image.url} className="App-logo" alt="logo" />
+        <p>{page.title}</p>
       </header>
     </div>
   );
